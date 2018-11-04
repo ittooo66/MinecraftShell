ca00f6bdb67bf1ab17ae000590ca78b89f4e9ea9
ito <ittooo66@gmail.com>
2018-11-04 16:59:52 +0900

    CPUリソース確認機能追加

--- MCCheckCPU.sh
+++ MCCheckCPU.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+#設定読み込み
+. /sf/shells/MCConf.sh
+
 while :
 do
 
-echo `ps -aux | grep spigot | grep -v grep | awk '{print $3}'`
-sleep 1
+CPUPER=`ps -aux | grep forge | grep -v grep | grep -v SCREEN | awk '{print $3}'`
+echo `date` CPU使用率 ${CPUPER}% >> ${MC_LOGDIR}/cpu.log
+sleep 10
 
 done
 
--- MCConf.sh
+++ MCConf.sh
@@ -12,3 +12,5 @@ MC_NOCHK=MCNotMonitor
 MC_JAR=forge-1.7.10-10.13.4.1558-1.7.10-universal.jar
 #Minecraftを起動させるScreenの名称
 SCREEN_NAME='minecraft'
+#Minecraftログファイル格納ディレクトリ
+MC_LOGDIR=/sf/logs
