57613a4fbf42521774da343a80df8833d98f2574
ito <ittooo66@gmail.com>
2018-11-26 01:44:28 +0900

    再起動時のtmpfsリストアを追加

--- MCStart.sh
+++ MCStart.sh
@@ -8,6 +8,11 @@ if [[ ! -z ${MC_SHELL} && ! -z ${MC_NOCHK} ]] ; then
   rm -f ${MC_SHELL}/${MC_NOCHK}
 fi
 
+#tmpfs初期化時は直近のtar.gzからリストア
+if [[ ! -d  ${MC_HOME} ]] ; then
+  ${MC_SHELL}/MCRestore.sh
+fi
+
 #Minecraft起動
 cd ${MC_HOME}
 screen -AmdS ${SCREEN_NAME} java -Xms8G -Xmx8G -jar ${MC_JAR}
