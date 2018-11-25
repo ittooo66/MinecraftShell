9f8f75d0e036f9f2ee0a9c42ed51b4739794b557
ito <ittooo66@gmail.com>
2018-11-26 01:43:15 +0900

    tar.gzでの圧縮化

--- MCBackup.sh
+++ MCBackup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#設定読み込み
+# 設定読み込み
 . /sf/shells/MCConf.sh
 
 #バックアップの実施
-cp -rp ${MC_HOME} ${MC_BACKUP}/`date +%Y_%m%d_%H%M`
+cd /
+tar cvfz ${MC_BACKUP}/`date +%Y_%m%d_%H%M`.tar.gz .${MC_HOME}
