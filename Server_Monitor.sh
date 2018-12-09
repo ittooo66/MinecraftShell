a97ecc7352f8bce14828ce13015ebe916b6f24de
ito <ittooo66@gmail.com>
2018-12-09 21:23:16 +0900

    デバッグ

--- Server_Monitor.sh
+++ Server_Monitor.sh
@@ -8,6 +8,6 @@ PROC_CHECK=`ps -ef | grep Server_Monitor.py | grep -v grep`
 
 # プロセスが存在しない場合、起動する
 if [[ -z ${PROC_CHECK} ]] ; then
-  ${TAS_SCRIPTS}/Server_SendMessageToDiscord.sh -w "サーバBOTが未起動のため、これより起動します"
-  python3.5 ${TAS_SCRIPTS}/Server_Monitor.py &
+  ${MCS_SCRIPTS}/Server_SendMessageToDiscord.sh -w "サーバBOTが未起動のため、これより起動します"
+  python3.5 ${MCS_SCRIPTS}/Server_Monitor.py &
 fi
